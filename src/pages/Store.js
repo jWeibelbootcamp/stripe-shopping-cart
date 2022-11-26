7f82bd338507f67efa794acca4ae65f9d15bea81
Jason Weibel <weibel.jason@gmail.com>
2022-11-26 18:47:03 -0500

    productStore and products in Store

--- src/pages/Store.js
+++ src/pages/Store.js
@@ -1,6 +1,20 @@
+import { Row, Col } from 'react-bootstrap';
+import { productArray } from '../productStore';
+
 function Store() {
     return (
-        <h1>Welcome, get Shopping!</h1>
+        <div>
+            <h1 align='center' className='p-3'>
+                Welcome, get Shopping!
+            </h1>
+            <Row xs={1} md={3} className='g-4'>
+                {productArray.map((product, index) => (
+                    <Col align='center' key={index}>
+                        <h1>{product.name}</h1>
+                    </Col>
+                ))}
+            </Row>
+        </div>
     );
 };
 
