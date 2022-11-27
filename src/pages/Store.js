673a7feb6e1e3efd7cf51f7a7dd34be5b7392285
Jason Weibel <weibel.jason@gmail.com>
2022-11-27 07:40:29 -0500

    product card

--- src/pages/Store.js
+++ src/pages/Store.js
@@ -10,7 +10,7 @@ function Store() {
             <Row xs={1} md={3} className='g-4'>
                 {productArray.map((product, index) => (
                     <Col align='center' key={index}>
-                        <h1>{product.name}</h1>
+                        <h2>{product.name}</h2>
                     </Col>
                 ))}
             </Row>
