e4a55530b9a37e53b5cf2a3a54a59a0d2d7a8370
devWeibel <weibel.jason@gmail.com>
2022-11-29 21:54:42 -0500

    stripe code entered but not fully working yet

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const stripe = require('stripe')('sk_test_51M9WEeA0zgGYE8hKfLzdebUdsNrrjNE3SI2bkSS8NclVm5VXPYz0VglrMEMnmJnK4uKi3jsQvBEkHMaFZEpSJsLr00EcdyU0Ss');
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+// const PORT = process.env.PORT || 3001;
 
 app.use(cors());
 app.use(express.static('public'));
@@ -12,7 +12,7 @@ app.use(express.json());
 app.post('/checkout', async (req, res) => {
     const items = req.body.items;
     let lineItems = [];
-    items.forEach((item) => {
+    items.forEach((item) => { // converts items to Stripe friendly format
         lineItems.push(
             {
                 price: item.id,
@@ -20,17 +20,18 @@ app.post('/checkout', async (req, res) => {
             }
         )
     });
-
+  
+    // creates session with items that Stripe can now read
     const session = await stripe.checkout.sessions.create({
         line_items: lineItems,
         mode: 'payment',
-        success_url: 'http://localhost:3000/success',
-        cancel_url: 'http://localhost:3000/success'
+        success_url: 'http://localhost:3000/success', // these addresses will need to change
+        cancel_url: 'http://localhost:3000/cancel'
     });
-
-    res.send(JSON.stringify({
+  
+    res.send(JSON.stringify({ // sends Stripe info back to front end
         url: session.url
     }));
-});
+  });
 
-app.listen(PORT, () => console.log(`Listening on port ${PORT}!`))
+app.listen(3000, () => console.log(`Listening on port 4000!`))
